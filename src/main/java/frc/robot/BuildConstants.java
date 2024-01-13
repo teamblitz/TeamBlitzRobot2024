f0858a77bb41f3b0b051045b812e34ed9f6fcc23
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2024-01-13 14:43:35 -0700

    Pain and suffering, part 1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,16 +4,16 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "TeamBlitzRobot2023";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 172;
-    public static final String GIT_SHA = "f87c62ad22f4c432ee2f92467baef8f915758069";
-    public static final String GIT_DATE = "2023-10-03 18:40:27 MST";
-    public static final String GIT_BRANCH = "dev";
-    public static final String BUILD_DATE = "2023-10-12 17:42:32 MST";
-    public static final long BUILD_UNIX_TIME = 1697157752306L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "TeamBlitzRobot2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "fc447a0ae1cedd41a209597d63462627931689ed";
+  public static final String GIT_DATE = "2024-01-13 13:46:56 MST";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2024-01-13 14:42:54 MST";
+  public static final long BUILD_UNIX_TIME = 1705182174227L;
+  public static final int DIRTY = 1;
 
-    private BuildConstants(){}
+  private BuildConstants(){}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,19 +23,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.ManipulatorCommandFactory;
 import frc.robot.commands.TeleopSwerve;
-import frc.robot.commands.arm.HoldArmAtPositionCommand;
-import frc.robot.commands.auto.AutonomousPathCommand;
-import frc.robot.subsystems.arm.ArmIOTalon;
-import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.subsystems.drive.SwerveModuleIOSparkMax;
 import frc.robot.subsystems.drive.gyro.GyroIONavx;
 import frc.robot.subsystems.drive.gyro.GyroIOPigeon;
-import frc.robot.subsystems.intake.IntakeIOSimple;
-import frc.robot.subsystems.intake.IntakeSubsystem;
-import frc.robot.subsystems.leds.LedSubsystem;
-import frc.robot.subsystems.wrist.WristIOSpark;
-import frc.robot.subsystems.wrist.WristSubsystem;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -49,10 +41,6 @@ public class RobotContainer {
 
     private DriveSubsystem driveSubsystem;
 
-    private ArmSubsystem armSubsystem;
-    private WristSubsystem wristSubsystem;
-    private IntakeSubsystem intakeSubsystem;
-    private LedSubsystem ledSubsystem;
 
     private ManipulatorCommandFactory manipulatorCommandFactory;
 
@@ -89,9 +77,6 @@ public class RobotContainer {
     public RobotContainer() {
         configureSubsystems();
 
-        manipulatorCommandFactory =
-                new ManipulatorCommandFactory(armSubsystem, intakeSubsystem, wristSubsystem);
-
         configureButtonBindings();
         setDefaultCommands();
 
@@ -123,11 +108,6 @@ public class RobotContainer {
                         () -> OIConstants.inputCurve.apply(-driveController.getTwist()) * .3,
                         () -> false,
                         () -> driveController.getPOV()));
-
-        armSubsystem.rotationRequirement.setDefaultCommand(
-                new HoldArmAtPositionCommand(armSubsystem));
-        wristSubsystem.setDefaultCommand(
-                Commands.waitSeconds(0).andThen(wristSubsystem.holdAtRelativeCommand()));
     }
 
     private final SlewRateLimiter driveMultiplierLimiter = new SlewRateLimiter(.25);
@@ -151,19 +131,6 @@ public class RobotContainer {
                         new SwerveModuleIOSparkMax(Constants.Swerve.Mod3.CONSTANTS),
                         Constants.Swerve.USE_PIGEON ? new GyroIOPigeon() : new GyroIONavx());
 
-        armSubsystem =
-                new ArmSubsystem(
-                        new ArmIOTalon(),
-                        () -> wristSubsystem.getRelativeRotation(),
-                        () -> wristSubsystem.getRotation());
-        wristSubsystem =
-                new WristSubsystem(
-                        new WristIOSpark(),
-                        () -> armSubsystem.getRotation(),
-                        () -> armSubsystem.getArmLength());
-        intakeSubsystem = new IntakeSubsystem(new IntakeIOSimple());
-        ledSubsystem = new LedSubsystem();
-
         driveController = new Joystick(0); // Move this to Controller (and I never did)
         controller = new Controller(0, 1);
     }
@@ -175,89 +142,9 @@ public class RobotContainer {
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
-        controller
-                .coneInTrigger()
-                .whileTrue(intakeSubsystem.buildConeInCommand())
-                .onFalse(intakeSubsystem.slowIntakeCommand());
-        controller.coneOutTrigger().whileTrue(intakeSubsystem.buildConeOutCommand());
-        controller
-                .cubeInTrigger()
-                .whileTrue(intakeSubsystem.buildCubeInCommand())
-                .onFalse(intakeSubsystem.slowIntakeCommand());
-        controller.cubeOutTrigger().whileTrue(intakeSubsystem.buildCubeOutCommand());
-        controller.slowIntakeTrigger().whileTrue(intakeSubsystem.slowIntakeCommand());
-
-        controller.wristLevelTrigger().onTrue(wristSubsystem.rotateRobotRelativeToCommand(0));
-        controller.wristDownTrigger().onTrue(wristSubsystem.rotateRobotRelativeToCommand(-90));
-
-        controller.signalCube().whileTrue(ledSubsystem.cubeSolid());
-        controller.signalCone().whileTrue(ledSubsystem.coneSolid());
-
+        
         controller.restGyroTrigger().onTrue(Commands.runOnce(driveSubsystem::zeroGyro));
         controller.xBrakeTrigger().onTrue(driveSubsystem.buildParkCommand());
-
-        new Trigger(() -> Math.abs(controller.getWristSpeed()) > .02)
-                .whileTrue(
-                        Commands.run(
-                                        () ->
-                                                wristSubsystem.setRotationSpeed(
-                                                        controller.getWristSpeed()),
-                                        wristSubsystem)
-                                .alongWith(
-                                        Commands.run(
-                                                () -> {
-                                                    wristSubsystem.lastRelativeGoal =
-                                                            wristSubsystem.getRelativeRotation();
-                                                    wristSubsystem.lastGoal =
-                                                            wristSubsystem.getRotation();
-                                                }))
-                                .finallyDo((b) -> wristSubsystem.setRotationSpeed(0)));
-
-        new Trigger(() -> Math.abs(controller.getArmSpeed()) > .02)
-                .whileTrue(
-                        Commands.run(
-                                () -> {
-                                    armSubsystem.setArmRotationSpeed(controller.getArmSpeed());
-                                },
-                                armSubsystem.rotationRequirement));
-
-        new Trigger(() -> Math.abs(controller.getExtensionSpeed()) > .05)
-                .whileTrue(
-                        Commands.run(
-                                        () -> {
-                                            armSubsystem.setArmExtensionSpeed(
-                                                    controller.getExtensionSpeed());
-                                        },
-                                        armSubsystem.extensionRequirement)
-                                .finallyDo((b) -> armSubsystem.setArmExtensionSpeed(0)));
-
-        controller.primeHybridTrigger().onTrue(manipulatorCommandFactory.primeHybrid());
-
-        controller.primeMidConeTrigger().onTrue(manipulatorCommandFactory.primeConeMid());
-        controller.primeHighConeTrigger().onTrue(manipulatorCommandFactory.primeConeHigh());
-
-        controller.primeMidCubeTrigger().onTrue(manipulatorCommandFactory.primeCubeMid());
-        controller.primeHighCubeTrigger().onTrue(manipulatorCommandFactory.primeCubeHigh());
-
-        controller.homeArmTrigger().onTrue(armSubsystem.homeArmCommand());
-        controller.retractArmTrigger().onTrue(armSubsystem.retractArmCommand());
-
-        controller.primeConeShelfTrigger().onTrue(manipulatorCommandFactory.primeConeShelf());
-        controller.primeCubeShelfTrigger().onTrue(manipulatorCommandFactory.primeCubeShelf());
-
-        controller.primeCubeRampTrigger().onTrue(manipulatorCommandFactory.primeCubeRamp());
-        controller.primeConeRampTrigger().onTrue(manipulatorCommandFactory.primeConeRamp());
-
-        controller.groundCubePickupTrigger().onTrue(manipulatorCommandFactory.groundCubePickup());
-        controller
-                .groundConeUprightPickupTrigger()
-                .onTrue(manipulatorCommandFactory.groundUprightConePickup());
-        controller
-                .groundConeFallenPickupTrigger()
-                .onTrue(manipulatorCommandFactory.groundFallenConePickup());
-
-        controller.cone().onTrue(Commands.runOnce(() -> SmartDashboard.putBoolean("mode", true)));
-        controller.cube().onTrue(Commands.runOnce(() -> SmartDashboard.putBoolean("mode", false)));
 
         controller
                 .brakeModeTrigger()
@@ -271,13 +158,11 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() { // Autonomous code goes here
-        Command wristFix = Commands.runOnce(wristSubsystem::setHoldGoals);
-        wristFix.schedule();
-
         String autoCommand = chooser.getSelected();
-        AutonomousPathCommand autonomousPathCommand =
-                new AutonomousPathCommand(
-                        driveSubsystem, armSubsystem, intakeSubsystem, manipulatorCommandFactory);
-        return autonomousPathCommand.generateAutonomous(autoCommand);
+    //     AutonomousPathCommand autonomousPathCommand =
+    //             new AutonomousPathCommand(
+    //                     driveSubsystem, armSubsystem, intakeSubsystem, manipulatorCommandFactory);
+    //     return autonomousPathCommand.generateAutonomous(autoCommand);
+        return null;
     }
 }
