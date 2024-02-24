a2fc2cf76e440dd02d03ee05d3a718e8aca8eefc
teamblitz2083 <73154888+teamblitz2083@users.noreply.github.com>
2024-02-24 15:17:18 -0700

    Speed tunings

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -251,7 +251,7 @@ public final class Constants {
             public static final double TRANSIT_STAGE = Units.degreesToRadians(15);
             public static final double TRANSIT_NORMAL = Units.degreesToRadians(60);
             public static final double SCORE_AMP = Units.degreesToRadians(100);
-            public static final double SCORE_SPEAKER = Units.degreesToRadians(20);
+            public static final double SCORE_SPEAKER = Units.degreesToRadians(30);
         }
     }
 
