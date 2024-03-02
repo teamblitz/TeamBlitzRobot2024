eedca07167548bde31b74433aae3814eb3ec01d7
Noah <40500696+NoahStricker@users.noreply.github.com>
2024-03-01 17:47:46 -0700

    Shooter sysid

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,19 +1,54 @@
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.lib.BlitzSubsystem;
 // import frc.robot.subsystems.intake.IntakeIO.IntakeIOInputs;
+import frc.robot.Constants;
 import frc.robot.subsystems.shooter.ShooterIO.ShooterIOInputs;
 import org.littletonrobotics.junction.Logger;
+
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
 
 public class Shooter extends SubsystemBase implements BlitzSubsystem {
 
     private final ShooterIO io;
     private final ShooterIOInputsAutoLogged inputs = new ShooterIOInputsAutoLogged();
 
+    private final SysIdRoutine routine;
+
     public Shooter(ShooterIO io) {
         this.io = io;
+
+
+        routine =
+                new SysIdRoutine(
+                        new SysIdRoutine.Config(
+                                null,
+                                null,
+                                null,
+                                (state) -> Logger.recordOutput("SysIdTestState", state.toString())),
+                        new SysIdRoutine.Mechanism(
+                                (volts) -> {
+                                    io.setVolts(volts.in(Volts));
+                                },
+                                null,
+                                this));
+
+        ShuffleboardTab tab = Shuffleboard.getTab("Sysid");
+        tab.add("ShooterQuasistaticFwd", sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+        tab.add("ShooterQuasistaticRev", sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+        
+        tab.add("ShooterDynamicFwd", sysIdDynamic(SysIdRoutine.Direction.kForward));
+        tab.add("ShooterDynamicRev", sysIdDynamic(SysIdRoutine.Direction.kReverse));
+
     }
 
     public void shootOpenLoop() {
@@ -45,4 +80,13 @@ public class Shooter extends SubsystemBase implements BlitzSubsystem {
     public Command reverseCommand() {
         return startEnd(this::reverse, this::stop);
     }
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+        return routine.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+        return routine.dynamic(direction);
+    }
+
 }
